1674a19a9d8ad47d05fb731850b4d7646df3cc18
Jack Rong <rongyadong19900724@gmail.com>
2019-03-24 18:38:03 +1100

    add styled-components features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../logo.svg'
 
 class Navbar extends Component {
-  state = {}
   render() {
     return (
-      <div>
-        <h3>Navbar</h3>
-      </div>
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="e-store-logo" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus" />
+            My cart
+          </button>
+        </Link>
+      </nav>
     )
   }
 }
