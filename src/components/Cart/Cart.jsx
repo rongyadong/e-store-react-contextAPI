d526748e217a2dce740cd729ce01318c91028fa5
Jack Rong <rongyadong19900724@gmail.com>
2019-03-28 00:56:08 +1100

    add mock Paypal component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Title from '../../util/Title'
 import CartColums from './CartColums'
 import EmptyCart from './EmptyCart'
@@ -6,26 +6,28 @@ import { ProductConsumer } from '../../context'
 import CartList from './CartList'
 import CartTotals from './CartTotals'
 
-const Cart = () => {
-  return (
-    <section>
-      <ProductConsumer>
-        {value => {
-          const { cart } = value
-          if (cart.length > 0)
-            return (
-              <React.Fragment>
-                <Title name="your" title="cart" />
-                <CartColums />
-                <CartList value={value} />
-                <CartTotals value={value} />
-              </React.Fragment>
-            )
-          return <EmptyCart />
-        }}
-      </ProductConsumer>
-    </section>
-  )
+class Cart extends Component {
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value
+            if (cart.length > 0)
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColums />
+                  <CartList value={value} />
+                  <CartTotals value={value} history={this.props.history} />
+                </React.Fragment>
+              )
+            return <EmptyCart />
+          }}
+        </ProductConsumer>
+      </section>
+    )
+  }
 }
 
 export default Cart
